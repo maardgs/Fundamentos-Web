21dc7b48fa3f4641dc84aa9bbc5aadaf5adec0ec
maardgs <mariliarodrigues.94@gmail.com>
2024-08-01 15:45:28 -0300

    :lipstick: feat: mapa

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,14 @@ por Seletor: querySelector()
 
 let nome = document.getElementById("nome")
 let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
 
 nome.style.width = "50%"
 email.style.width = "50%"
+assunto.style.width = "100%"
 
 function validaNome() {
     let txtNome = document.querySelector("#txtNome")
@@ -23,6 +28,7 @@ function validaNome() {
     } else {
         txtNome.innerHTML = 'Nome Válido'
         txtNome.style.color = 'green'
+        nomeOk = true
     }
 
 }
@@ -36,6 +42,29 @@ function validaEmail(){
     }else{
         txtEmail.innerHTML = "E-mail válido"
         txtEmail.style.color = 'green'
+        emailOk = true
     }
 }
 
+function validaAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if (assunto.value.length >=100 ){
+        txtAssunto.innerHTML = "Texto é muito grande, digite no máximo 100 caracteres"
+        txtAssunto.style.color = "red"
+        txtAssunto.style.display = "block"
+    } else {
+        txtAssunto.style.display = "none"
+        assuntoOk = true
+
+    }
+
+}
+
+function enviar(){
+if (nomeOk == true && emailOk == true && assuntoOk == true){
+    alert ("Formulário enviado com sucesso!")
+    } else{
+        alert("Preencha o formulário corretamente antes de enviar...")
+    }
+}
